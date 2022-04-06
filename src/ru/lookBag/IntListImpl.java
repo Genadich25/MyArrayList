a1c7f7c02fb0b772a40aa5d7e3b63558379c4e87
pin20 <pin200048@gmail.com>
2022-04-06 18:05:15 +0300

    Готовченко

--- src/ru/lookBag/IntListImpl.java
+++ src/ru/lookBag/IntListImpl.java
@@ -1,11 +1,11 @@
 package ru.lookBag;
 
-public interface StringListInt {
+public interface IntListImpl {
 
     // Добавление элемента.
     // Вернуть добавленный элемент
     // в качестве результата выполнения.
-    String add(String item);
+    int add(int item);
 
     // Добавление элемента
     // на определенную позицию списка.
@@ -14,7 +14,7 @@ public interface StringListInt {
     // выбросить исключение.
     // Вернуть добавленный элемент
     // в качестве результата выполнения.
-    String add(int index, String item);
+    int add(int index, int item);
 
     // Установить элемент
     // на определенную позицию,
@@ -23,44 +23,44 @@ public interface StringListInt {
     // если индекс больше
     // фактического количества элементов
     // или выходит за пределы массива.
-    String set(int index, String item);
+    int set(int index, int item);
 
     // Удаление элемента.
     // Вернуть удаленный элемент
     // или исключение, если подобный
     // элемент отсутствует в списке.
-    String remove(String item);
+    int remove(int item);
 
     // Удаление элемента по индексу.
     // Вернуть удаленный элемент
     // или исключение, если подобный
     // элемент отсутствует в списке.
-    String remove(int index);
+    int removeForIndex(int index);
 
     // Проверка на существование элемента.
     // Вернуть true/false;
-    boolean contains(String item);
+    boolean contains(int[] arr,int item);
 
     // Поиск элемента.
     // Вернуть индекс элемента
     // или -1 в случае отсутствия.
-    int indexOf(String item);
+    int indexOf(int item);
 
     // Поиск элемента с конца.
     // Вернуть индекс элемента
     // или -1 в случае отсутствия.
-    int lastIndexOf(String item);
+    int lastIndexOf(int item);
 
     // Получить элемент по индексу.
     // Вернуть элемент или исключение,
     // если выходит за рамки фактического
     // количества элементов.
-    String get(int index);
+    int get(int index);
 
     // Сравнить текущий список с другим.
     // Вернуть true/false или исключение,
     // если передан null.
-    boolean equals(StringList otherList);
+    boolean equals(IntList otherList);
 
     // Вернуть фактическое количество элементов.
     int size();
@@ -76,5 +76,6 @@ public interface StringListInt {
     // Создать новый массив
     // из строк в списке
     // и вернуть его.
-    String[] toArray();
+    int[] toArray();
+
 }
